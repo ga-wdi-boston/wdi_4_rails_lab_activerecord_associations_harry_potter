331c0bdc4f4a2b12f174b065acede3db03c634a8
Michael Finneran <mrfinneran@gmail.com>
2013-10-17 16:16:12 -0400

    Forked and created Rails app

---
+++ HappiHogwarts/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HappiHogwarts::Application.config.secret_key_base = '4256b829f13cc15e261142d3cbb02db6205a9c07cf7ec93e7cd776dc9090b537866e9b845acbb7b29d77a253c2e6381a35e9e9bfa2ade7a9be24c6140da82d1f'

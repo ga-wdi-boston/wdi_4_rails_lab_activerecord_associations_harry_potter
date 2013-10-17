bff27fca0e65198541a7428bb057bfb6ad4dcdda
Michael Finneran <mrfinneran@gmail.com>
2013-10-17 16:40:20 -0400

    Migrate house_id to student.

--- HappiHogwarts/db/schema.rb
+++ HappiHogwarts/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131017202006) do
+ActiveRecord::Schema.define(version: 20131017203859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20131017202006) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "house_id"
   end
 
 end

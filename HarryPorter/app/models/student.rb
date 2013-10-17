73d5a30b07de2004170ed63812210d5eb1f21133
Amal Hussein <amalhussein08@gmail.com>
2013-10-17 17:53:11 -0400

    Added student info to seed.db

---
+++ HarryPorter/app/models/student.rb
@@ -0,0 +1,9 @@
+class Student <  ActiveRecord::Base 
+	belongs_to :house 
+end
+
+
+
+
+
+
--- HarryPorter/db/seeds.rb
+++ HarryPorter/db/seeds.rb
@@ -14,6 +14,12 @@ House.create(name: "Slytherin", colors: "green, silver", slogan: "Slytherin will
 House.create(name: "Griffindor", colors: "red, gold", slogan: "Their daring, nerve and chivalry set Gryffindors apart")
 
 
+Student.create(name: "Harry Potter", house_id: 4) 
+Student.create(name: "Draco Malfoy", house_id: 3) 
+Student.create(name: "Hermione Granger", house_id: 4) 
+Student.create(name: "Luna Lovegood", house_id: 1) 
+Student.create(name: "Robert Pattinson", house_id: 2)
+
 
 
 

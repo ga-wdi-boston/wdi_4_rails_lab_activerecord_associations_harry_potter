3edbfc38b39ad198ea31f7eaea5465939ccf10d1
Bobby Mazzotti <robert.mazzotti@gmail.com>
2013-10-17 16:28:43 -0400

    Setup intial stucture rails app

---
+++ hogwarts_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HogwartsApp::Application.config.secret_key_base = 'c2f1f1034321cd0eb8eaf5dc46d41f104a7b0785b14f1b3e62cca181524ba9771583ea8566b2767f3bab46b94d87be1ad91229913ba8715ca67c4ba7c0844b39'

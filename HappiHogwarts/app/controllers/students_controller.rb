ca3609f48ebfa08783d79ca947445dd9ef162724
Michael Finneran <mrfinneran@gmail.com>
2013-10-17 17:17:23 -0400

    Added routes students, created crud actions for students in controller, created view show for students

---
+++ HappiHogwarts/app/controllers/students_controller.rb
@@ -0,0 +1,45 @@
+class StudentsController < ApplicationController
+before_action :set_student, only: [:show, :edit, :update, :destroy]
+
+
+def show
+end
+
+def index
+  @students = Student.all
+end
+
+def create
+  @student = Student.new(student_params)
+end
+
+def new
+  @student = Student.new
+end
+
+def edit
+end
+
+def update
+  if @student.update(student_params)
+    redirect_to @student 
+  else
+    render action: 'edit'
+  end
+end
+
+def destroy
+  @student.destroy
+end
+
+private
+
+def set_student
+@student = student.find(params[:id])
+end
+
+def student_params
+  params.require(:student).permit(:name, :house_id)
+end
+
+end
--- HappiHogwarts/config/routes.rb
+++ HappiHogwarts/config/routes.rb
@@ -1,3 +1,4 @@
 HappiHogwarts::Application.routes.draw do
   resources :houses
+  resources :students
 end

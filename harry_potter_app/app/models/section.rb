44e66ea01dc2d851234b5aab87f96a408d4ab202
Ben Winter <ben.g.winter@gmail.com>
2013-10-17 18:09:39 -0400

    changed section, professor  and students realationship

--- harry_potter_app/app/models/section.rb
+++ harry_potter_app/app/models/section.rb
@@ -1,5 +1,5 @@
 class Section < ActiveRecord::Base
   belongs_to :lecture
-  belongs_to :professor
-  belongs_to :student
+  has_many_and_belongs_to :professor
+  has_many_and_belongs_to :student
 end
--- harry_potter_app/db/migrate/20131017200323_create_professors.rb
+++ harry_potter_app/db/migrate/20131017200323_create_professors.rb
@@ -4,6 +4,7 @@ class CreateProfessors < ActiveRecord::Migration
       t.string :first_name
       t.string :last_name
       t.string :image
+      t.belongs_to :section, index:true
       t.timestamps
     end
   end

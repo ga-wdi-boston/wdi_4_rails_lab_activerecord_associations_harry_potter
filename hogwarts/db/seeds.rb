46f56c0410c12924eae571fa5c5886cd4e5ec0fa
Abby Howell <abby.g.howell@gmail.com>
2013-10-17 16:52:01 -0400

    made seed file for houses

--- hogwarts/db/seeds.rb
+++ hogwarts/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+House.create(name: "Ravenclaw", colors: "blue, bronze", slogan: "Wit beyond measure is man's greatest treasure")
+House.create(name: "Hufflepuff", colors: "yellow, black", slogan: "Those patient Hufflepuffs are true and unafraid of toil")
+House.create(name: "Slytherin", colors: "green, silver", slogan: "Slytherin will help you on your way to greatness")
+House.create(name: "Griffindor", colors: "red, gold", slogan: "Their daring, nerve and chivalry set Gryffindors apart")

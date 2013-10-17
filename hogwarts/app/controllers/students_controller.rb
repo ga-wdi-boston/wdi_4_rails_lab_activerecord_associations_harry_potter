3ace0953cb6368533b6431c088096bb23f0427e3
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-10-17 18:16:07 -0400

    added new actions to the student controller within the new and create methods

--- hogwarts/app/controllers/students_controller.rb
+++ hogwarts/app/controllers/students_controller.rb
@@ -14,9 +14,16 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    @student = Student.new(students_params)
+      if @student.save
+        redirect_to @student
+      else
+        render action: 'new'
+      end
   end
 
   def update

afa0a52f8e742abf3df2d91bc961a12523b45473
Rahul Horé <hore.rahul@gmail.com>
2013-10-17 16:42:06 -0400

    Creates Students migration

--- hogwarts_app/db/schema.rb
+++ hogwarts_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131017203257) do
+ActiveRecord::Schema.define(version: 20131017203808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20131017203257) do
   create_table "houses", force: true do |t|
     t.string   "name"
     t.string   "colors"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "students", force: true do |t|
+    t.string   "name"
+    t.integer  "year",       default: 1
     t.datetime "created_at"
     t.datetime "updated_at"
   end

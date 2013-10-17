c066382fdfe07fad8a18ec190b74dc7e668019e4
Rahul Horé <hore.rahul@gmail.com>
2013-10-17 18:06:17 -0400

    Creates Skills join table

--- hogwarts_app/app/models/professor.rb
+++ hogwarts_app/app/models/professor.rb
@@ -2,4 +2,5 @@ class Professor < ActiveRecord::Base
   has_many :spells
   has_many :lectures
   has_many :students through: :lectures
+  has_and_belongs_to_many :skills
 end
--- hogwarts_app/app/models/student.rb
+++ hogwarts_app/app/models/student.rb
@@ -3,4 +3,5 @@ class Student < ActiveRecord::Base
   belongs_to :house
   has_many :lectures
   has_many :professors, through: :lectures
+  has_and_belongs_to_many :skills
 end

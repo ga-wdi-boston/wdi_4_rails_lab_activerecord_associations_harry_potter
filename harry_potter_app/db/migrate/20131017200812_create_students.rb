b738e39a5201ba1113baad29ca12ca8ab0b0c8fd
Ben Winter <ben.g.winter@gmail.com>
2013-10-17 16:08:23 -0400

    added create students migration

---
+++ harry_potter_app/db/migrate/20131017200812_create_students.rb
@@ -0,0 +1,11 @@
+class CreateStudents < ActiveRecord::Migration
+  def change
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
+      t.integer :grade
+      t.string :gender
+      t.string :image
+    end
+  end
+end

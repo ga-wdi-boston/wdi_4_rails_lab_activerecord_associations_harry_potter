4fc81238d6dc524f25e73ced46eb3de23015bb84
Amal Hussein <amalhussein08@gmail.com>
2013-10-17 17:04:53 -0400

    Initial Setup=pre:migration

---
+++ HarryPorter/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HarryPorter::Application.config.secret_key_base = '6e937e70de0b3269d27627423e57d082513e6d296f188396883224883d308848617178f085e7320b8bfc81e0eedbffbf2d18849cf1f2f24d43589b3957ca491d'

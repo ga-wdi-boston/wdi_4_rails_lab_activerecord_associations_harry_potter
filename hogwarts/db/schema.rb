804d1efe2532a7944decea19ef1b0be06360daa2
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-10-17 19:53:17 -0400

    the teachers have migrated south for the winter

--- hogwarts/db/schema.rb
+++ hogwarts/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131017231201) do
+ActiveRecord::Schema.define(version: 20131017235139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,5 +44,12 @@ ActiveRecord::Schema.define(version: 20131017231201) do
   end
 
   add_index "students", ["house_id"], name: "index_students_on_house_id", using: :btree
+
+  create_table "teachers", force: true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end

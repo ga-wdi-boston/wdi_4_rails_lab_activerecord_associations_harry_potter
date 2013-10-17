2294d82c1aa323e252d3f4517a403cd97853126d
Rahul Horé <hore.rahul@gmail.com>
2013-10-17 17:39:44 -0400

    Migrates Spells

--- hogwarts_app/db/schema.rb
+++ hogwarts_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131017212204) do
+ActiveRecord::Schema.define(version: 20131017213238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,12 @@ ActiveRecord::Schema.define(version: 20131017212204) do
 
   create_table "sections", force: true do |t|
     t.string   "day"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "spells", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end

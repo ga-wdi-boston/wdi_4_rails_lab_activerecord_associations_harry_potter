617b8a19e58118763578fb1f59fd6e1718fed30e
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-10-17 19:55:43 -0400

    generated controller for teachers.

--- hogwarts/config/routes.rb
+++ hogwarts/config/routes.rb
@@ -1,4 +1,11 @@
 Hogwarts::Application.routes.draw do
+  get "teachers/index"
+  get "teachers/show"
+  get "teachers/new"
+  get "teachers/create"
+  get "teachers/edit"
+  get "teachers/update"
+  get "teachers/destroy"
 resources :students
 resources :lectures
 resources :houses, only: [:show, :index] do

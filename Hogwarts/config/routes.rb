31f0950f3c2b51ee15e67537fd7c02b54c9d7394
Matt Clement <darkmoves@gmail.com>
2013-10-17 17:27:37 -0400

    Add Sections and Lectures to routes.

--- Hogwarts/config/routes.rb
+++ Hogwarts/config/routes.rb
@@ -2,6 +2,12 @@ Hogwarts::Application.routes.draw do
   resources :students
 
   resources :professors
+
+  resources :sections
+
+  resources :lectures do
+  	resources :sections
+  end
   
   resources :houses do
     resources :students

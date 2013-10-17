04d130a08222db8568b82cf1b0842dfa4b9279ee
Ben Winter <ben.g.winter@gmail.com>
2013-10-17 19:38:25 -0400

    added controller for lectures

---
+++ harry_potter_app/app/controllers/lectures_controller.rb
@@ -0,0 +1,63 @@
+class LecturesController < ApplicationController
+  before_action :set_lecture, only: [:show, :edit, :update, :destroy]
+
+  # GET /lectures
+  def index
+    @lectures = Lecture.all
+  end
+
+  # GET /lectures/1
+  def show
+  end
+
+  # GET /lectures/new
+  def new
+    @lecture = Lecture.new
+  end
+
+  # GET /lectures/1/edit
+  def edit
+  end
+
+  # POST /lectures
+  def create
+    @lecture = Lecture.new(section_params)
+
+    respond_to do |format|
+      if @lecture.save
+        format.html { redirect_to @lecture, notice: 'Lecture was successfully added.' }
+      else
+        format.html { render action: 'new' }
+      end
+    end
+  end
+
+  # PATCH/PUT /lectures/1
+  def update
+    respond_to do |format|
+      if @lecture.update(section_params)
+        format.html { redirect_to @lecture, notice: 'Lecture was successfully updated.' }
+      else
+        format.html { render action: 'edit' }
+      end
+    end
+  end
+
+  # DELETE /lectures/1
+  def destroy
+    @lecture.destroy
+    respond_to do |format|
+      format.html { redirect_to lectures_url }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_lecture
+      @lecture = Lecture.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def lecture_params
+      params.require(:lecture).permit(:name, :location)
+end

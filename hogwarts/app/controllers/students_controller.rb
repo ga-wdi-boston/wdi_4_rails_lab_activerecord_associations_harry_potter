b44fc698243758cfb0f70549e6ba13ddd5202491
Abby Howell <abby.g.howell@gmail.com>
2013-10-17 18:15:42 -0400

    added new and create actions to students_controller

--- hogwarts/app/controllers/students_controller.rb
+++ hogwarts/app/controllers/students_controller.rb
@@ -13,9 +13,16 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    @student = Student.new(student_params)
+    if @student.save
+      redirect_to @student
+    else
+      render action: 'new'
+    end
   end
 
   def edit

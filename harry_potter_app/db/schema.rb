2e9f7310e77c40d4e7b82895b7a9934af4def046
Ben Winter <ben.g.winter@gmail.com>
2013-10-17 16:11:21 -0400

    added lectures migration and lecture.rb

--- harry_potter_app/db/schema.rb
+++ harry_potter_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131017200323) do
+ActiveRecord::Schema.define(version: 20131017200812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20131017200323) do
   create_table "professors", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "students", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "grade"
+    t.string   "gender"
     t.string   "image"
     t.datetime "created_at"
     t.datetime "updated_at"

18b086a7e0eaf2ef7ce5b887daa896849823324f
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-10-17 18:00:58 -0400

    generated student controller

--- hogwarts/config/routes.rb
+++ hogwarts/config/routes.rb
@@ -1,3 +1,9 @@
 Hogwarts::Application.routes.draw do
+  get "students/index"
+  get "students/show"
+  get "students/new"
+  get "students/create"
+  get "students/update"
+  get "students/destroy"
 resources :houses, only: [:show, :index]
 end

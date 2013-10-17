21f7da94c0dc751844f4b24154a04e3a01d4dbb0
Amal Hussein <amalhussein08@gmail.com>
2013-10-17 18:16:09 -0400

    Added actions to the create method for students

--- HarryPorter/app/controllers/students_controller.rb
+++ HarryPorter/app/controllers/students_controller.rb
@@ -2,9 +2,9 @@ class StudentsController < ApplicationController
   before_action :set_student, only: [:show, :edit, :update, :destroy]
 
   def index
-      if params[:house_id]
-        @students = Student.where(:house_id, params[:house_id])
-      else 
+    if params[:house_id]
+      @students = Student.where(:house_id, params[:house_id])
+    else 
       @students = Student.all 
     end 
   end
@@ -13,9 +13,16 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @students = Student.new
   end
 
   def create
+    @student = Student.new(student_params)
+    if @student.save 
+      redirect_to @student
+    else 
+      render action: 'new'
+    end 
   end
 
   def edit
@@ -27,14 +34,14 @@ class StudentsController < ApplicationController
   def destroy
   end
 
-private 
+  private 
 
-def set_student
-  @student = Student.find(params[:id])
-end 
+  def set_student
+    @student = Student.find(params[:id])
+  end 
 
-def student_params
-  params.require(:student).permit(:name, :house_id)
-end 
+  def student_params
+    params.require(:student).permit(:name, :house_id)
+  end 
 
 end

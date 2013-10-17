4198c4c55c2ea65f4bb43094c3f2e8e57a37c5ff
Matt Clement <darkmoves@gmail.com>
2013-10-17 16:42:35 -0400

    Created Students migration and performed migration for Houses and Students.

--- Hogwarts/db/migrate/20131017203809_create_students.rb
+++ Hogwarts/db/migrate/20131017203809_create_students.rb
@@ -2,7 +2,8 @@ class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
       t.string :name
-      t.integer :year
+      t.integer :year, default: 1
+      t.timestamps
     end
   end
 end

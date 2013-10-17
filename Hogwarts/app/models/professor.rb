b1dd7b173bb577e2d0485754722739ae82a10b92
Matt Clement <darkmoves@gmail.com>
2013-10-17 17:35:44 -0400

    Added CreateSpell migration, edited Proessor, Student and Spell models to reflect relationships.

--- Hogwarts/app/models/professor.rb
+++ Hogwarts/app/models/professor.rb
@@ -1,3 +1,4 @@
 class Professor < ActiveRecord::Base
-	
+	has_many :spells
+	has_many :students, through :sections	
 end
--- Hogwarts/app/models/student.rb
+++ Hogwarts/app/models/student.rb
@@ -1,3 +1,5 @@
 class Student < ActiveRecord::Base
 	belongs_to :house
+	has_many :professors, through :sections
+	has_many :spells
 end

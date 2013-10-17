7b224c0b4397c0939c586ce4b357ecd97bb4cbb5
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-10-17 18:11:48 -0400

    added actions to student controller.rb. also added nesting action

--- hogwarts/app/controllers/students_controller.rb
+++ hogwarts/app/controllers/students_controller.rb
@@ -1,5 +1,13 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: [:edit, :show, :update, :destroy]
   def index
+    #this is very important for nesting 
+    if params[:house_id]
+      @students = Student.where(house_id: params[:house_id])
+    else
+      @students = Student.all
+    end
+
   end
 
   def show
@@ -16,4 +24,18 @@ class StudentsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def set_student
+    @students = Student.find(params[:id])
+  end
+
+  def students_params
+    params.require(:student).permit(:name, :house_id)
+  end
+
 end
+
+
+

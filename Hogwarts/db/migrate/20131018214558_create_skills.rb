975fe8edd85707510bee1390e3e5ddf88d195a11
Matt Clement <darkmoves@gmail.com>
2013-10-18 18:07:28 -0400

    Edited Skills Migration and Model to belong_to Spells.

--- Hogwarts/db/migrate/20131018214558_create_skills.rb
+++ Hogwarts/db/migrate/20131018214558_create_skills.rb
@@ -2,6 +2,7 @@ class CreateSkills < ActiveRecord::Migration
   def change
     create_table :skills do |t|
 	    t.integer :level
+	    t.belongs_to :spell
 	    t.belongs_to :skillable, polymorphic: true, index: true
 	    t.timestamps
     end

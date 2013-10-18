217bbce17aa04a284fb6271f03ce3fd68c2c2731
Dan Crews <dancrews@me.com>
2013-10-17 21:36:10 -0400

    added students table and student model.

--- hogwarts_sim/db/schema.rb
+++ hogwarts_sim/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131018004151) do
+ActiveRecord::Schema.define(version: 20131018013129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 20131018004151) do
     t.string "name"
     t.string "colors"
     t.string "slogan"
+  end
+
+  create_table "students", force: true do |t|
+    t.string "first_name"
+    t.string "last_name"
   end
 
 end

5441d38746c80a98a7176dc989c85c2be5536a67
Matt Clement <darkmoves@gmail.com>
2013-10-17 20:32:21 -0400

    Executed migration to add house_id to students and ran rake db:seed.

--- Hogwarts/db/schema.rb
+++ Hogwarts/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131017215457) do
+ActiveRecord::Schema.define(version: 20131018002950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20131017215457) do
     t.integer  "year",       default: 1
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "house_id"
   end
 
 end

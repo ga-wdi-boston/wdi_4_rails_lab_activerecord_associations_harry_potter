1b32f20922ad60e75babdf8dcf519d40a3709ec8
Ben Winter <ben.g.winter@gmail.com>
2013-10-18 17:31:50 -0400

    added ability to attribute skills to student when added

--- harry_potter_app/app/controllers/student_skills_controller.rb
+++ harry_potter_app/app/controllers/student_skills_controller.rb
@@ -4,6 +4,7 @@ class StudentSkillsController < ApplicationController
   # GET /student_skills/new
   def new
     @student_skill = StudentSkill.new
+    @student_id = params[:student_id]
   end
 
   # GET /student_skills/1/edit
@@ -13,7 +14,6 @@ class StudentSkillsController < ApplicationController
   # POST /student_skills
   def create
     @student_skill = StudentSkill.new(student_skill_params)
-
     respond_to do |format|
       if @student_skill.save
         format.html { redirect_to student_path(@student_skill.student_id), notice: 'A student skill was successfully added.' }
@@ -50,6 +50,6 @@ class StudentSkillsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def student_skill_params
-      params.require(:level, :student_id).permit(:level, :student_id) 
+      params.require(:student_skill).permit(:level, :student_id, :spell_id) 
     end
 end
--- harry_potter_app/app/controllers/students_controller.rb
+++ harry_potter_app/app/controllers/students_controller.rb
@@ -60,6 +60,6 @@ class StudentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def student_params
-      params.require(:student).permit(:first_name, :last_name, :grade, :gender, :image, :house_id)
+      params.require(:student).permit(:first_name, :last_name, :grade, :gender, :image, :house_id, student_skillsß)
     end
 end

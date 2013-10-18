7eff06aaeef8b7bf6c2ec8ffabfe51a8ebd442ee
Abby Howell <abby.g.howell@gmail.com>
2013-10-17 20:39:34 -0400

    migration successful

--- hogwarts/db/schema.rb
+++ hogwarts/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131018002403) do
+ActiveRecord::Schema.define(version: 20131018003629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 20131018002403) do
 
   add_index "lectures", ["student_id"], name: "index_lectures_on_student_id", using: :btree
   add_index "lectures", ["teacher_id"], name: "index_lectures_on_teacher_id", using: :btree
+
+  create_table "lectures_students", id: false, force: true do |t|
+    t.integer "lecture_id"
+    t.integer "student_id"
+  end
+
+  add_index "lectures_students", ["lecture_id", "student_id"], name: "index_lectures_students_on_lecture_id_and_student_id", using: :btree
+  add_index "lectures_students", ["lecture_id"], name: "index_lectures_students_on_lecture_id", using: :btree
+  add_index "lectures_students", ["student_id"], name: "index_lectures_students_on_student_id", using: :btree
 
   create_table "students", force: true do |t|
     t.integer  "house_id"

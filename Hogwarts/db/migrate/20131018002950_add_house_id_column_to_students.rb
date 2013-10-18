59f8f3fd470d7e1dfedbfa03a4bb3bf08d51076b
Matt Clement <darkmoves@gmail.com>
2013-10-18 17:49:07 -0400

    Created Skills migration. Deleted AddHouseIdColumnToStudents migration. Modified Students migration to include a :house_id.

--- Hogwarts/db/migrate/20131018002950_add_house_id_column_to_students.rb
+++
@@ -1,5 +0,0 @@
-class AddHouseIdColumnToStudents < ActiveRecord::Migration
-  def change
-    add_column :students, :house_id, :integer
-  end
-end
---
+++ Hogwarts/db/migrate/20131018214558_create_skills.rb
@@ -0,0 +1,9 @@
+class CreateSkills < ActiveRecord::Migration
+  def change
+    create_table :skills do |t|
+	    t.integer :level
+	    t.belongs_to :skillable, polymorphic: true, index: true
+	    t.timestamps
+    end
+  end
+end

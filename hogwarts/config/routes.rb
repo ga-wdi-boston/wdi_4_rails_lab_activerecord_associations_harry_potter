3b39867f598d4475345892b5d2d5ce164e3abee7
Abby Howell <abby.g.howell@gmail.com>
2013-10-18 08:46:02 -0400

    updated resources to try nesting

--- hogwarts/config/routes.rb
+++ hogwarts/config/routes.rb
@@ -1,13 +1,16 @@
 Hogwarts::Application.routes.draw do
-  resources :teachers
   
-  resources :students
-
-  resources :lectures
+  resources :teachers do 
+  	resources :skills
+  	resources :lectures
+  end
+  
+  resources :students do
+  	resources :skills
+  	resources :lectures
+  end
 
   resources :spells
-
-  resources :skills
 
   resources :houses, only: [:show, :index] do
     resources :students

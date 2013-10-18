c8d81b96b2681afb7b046a42164e5817ff5aa900
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-10-17 20:07:24 -0400

    reformatted teacher routes

--- hogwarts/config/routes.rb
+++ hogwarts/config/routes.rb
@@ -1,11 +1,5 @@
 Hogwarts::Application.routes.draw do
-  get "teachers/index"
-  get "teachers/show"
-  get "teachers/new"
-  get "teachers/create"
-  get "teachers/edit"
-  get "teachers/update"
-  get "teachers/destroy"
+resources :teachers
 resources :students
 resources :lectures
 resources :houses, only: [:show, :index] do

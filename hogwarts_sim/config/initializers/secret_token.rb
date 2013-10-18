19bbd32202b2835b2eca35f9f540c182621c47e0
Dan Crews <dancrews@me.com>
2013-10-17 20:19:31 -0400

    Initial commit of Rails hogwarts_sim directory. Edited database.yml file and added pry-rails to Gemfile.

---
+++ hogwarts_sim/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HogwartsSim::Application.config.secret_key_base = '6fcb53ca76c118e0e05e170bd62617f25c291fd94edb797f76f314c9b221664abca23a1cd5f91cdb20036c4250dde2d308b9a7be4013f07d74566ccad32c7a78'
---
+++ hogwarts_sim/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HogwartsSim::Application.config.session_store :cookie_store, key: '_hogwarts_sim_session'

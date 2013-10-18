2cd2eaa624a4200cb44f386669c82b6b585c0d7f
Michael Finneran <mrfinneran@gmail.com>
2013-10-18 00:25:09 -0400

    created migration AddStudentIDToLEctures

--- HappiHogwarts/db/schema.rb
+++ HappiHogwarts/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131018035222) do
+ActiveRecord::Schema.define(version: 20131018042440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20131018035222) do
     t.integer "professor_id"
     t.string  "weekday"
     t.string  "topic"
+    t.integer "student_id"
   end
 
   create_table "lectures_students", id: false, force: true do |t|

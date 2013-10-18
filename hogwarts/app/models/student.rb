3d2b74dc353a4865a6638701fdf570369ec54536
Abby Howell <abby.g.howell@gmail.com>
2013-10-18 10:08:54 -0400

    updated checkboxes - has-many associations coming through the form now

--- hogwarts/app/models/student.rb
+++ hogwarts/app/models/student.rb
@@ -1,7 +1,6 @@
 class Student < ActiveRecord::Base
   belongs_to :house
   has_and_belongs_to_many :lectures
-  accepts_nested_attributes_for :lectures
   has_many :skills
   has_many :spells, through: :skills
 end

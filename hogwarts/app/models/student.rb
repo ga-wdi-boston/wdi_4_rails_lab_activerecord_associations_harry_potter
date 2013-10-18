dadb7c6fc338bf7f0f3f32c5ad6ee38a847aef5e
Abby Howell <abby.g.howell@gmail.com>
2013-10-17 22:13:32 -0400

    updated complex relationships between students, teacher, spells, and skills

--- hogwarts/app/models/student.rb
+++ hogwarts/app/models/student.rb
@@ -1,4 +1,6 @@
 class Student < ActiveRecord::Base
   belongs_to :house
   has_and_belongs_to_many :lectures
+  has_many :spells, through: :skills
+  has_many :skills
 end
--- hogwarts/app/models/teacher.rb
+++ hogwarts/app/models/teacher.rb
@@ -1,3 +1,5 @@
 class Teacher < ActiveRecord::Base
   has_many :lectures
+  has_many :spells, through: :skills
+  has_many :skills
 end

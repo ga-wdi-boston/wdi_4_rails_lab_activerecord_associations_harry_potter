3e4fb9e187c7d7a251432ee7bdd892ed8b63b30a
Abby Howell <abby.g.howell@gmail.com>
2013-10-17 20:25:04 -0400

    added migration file

--- hogwarts/db/migrate/20131018002403_update_lectures_teacher_id_student_id.rb
+++ hogwarts/db/migrate/20131018002403_update_lectures_teacher_id_student_id.rb
@@ -1,4 +1,6 @@
 class UpdateLecturesTeacherIdStudentId < ActiveRecord::Migration
   def change
+    rename_column(:lectures, :Teacher_id, :teacher_id)
+    rename_column(:lectures, :Student_id, :student_id)
   end
 end

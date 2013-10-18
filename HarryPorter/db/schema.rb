3fd3e5dd20bcc131ccead98dba940339056e4db3
Amal Hussein <amalhussein08@gmail.com>
2013-10-17 20:42:51 -0400

    completed joint table migration

--- HarryPorter/db/schema.rb
+++ HarryPorter/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131018002448) do
+ActiveRecord::Schema.define(version: 20131018003709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 20131018002448) do
 
   add_index "lectures", ["student_id"], name: "index_lectures_on_student_id", using: :btree
   add_index "lectures", ["teacher_id"], name: "index_lectures_on_teacher_id", using: :btree
+
+  create_table "lectures_students", id: false, force: true do |t|
+    t.integer "lecture_id"
+    t.integer "student_id"
+  end
+
+  add_index "lectures_students", ["lecture_id"], name: "index_lectures_students_on_lecture_id", using: :btree
+  add_index "lectures_students", ["student_id", "lecture_id"], name: "index_lectures_students_on_student_id_and_lecture_id", using: :btree
+  add_index "lectures_students", ["student_id"], name: "index_lectures_students_on_student_id", using: :btree
 
   create_table "students", force: true do |t|
     t.integer "house_id"

fe36a70e8895a2c7d16cf300a48d50f5f5252f84
Cori Allen <coriallen@gmail.com>
2013-10-18 08:47:20 -0400

    Create working show spells in student show view.

--- HappiHogwarts/db/schema.rb
+++ HappiHogwarts/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131018122517) do
+ActiveRecord::Schema.define(version: 20131018123425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20131018122517) do
     t.integer  "professor_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "incantation"
   end
 
   add_index "spells", ["professor_id"], name: "index_spells_on_professor_id", using: :btree

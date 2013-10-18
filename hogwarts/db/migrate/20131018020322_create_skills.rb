cfae1ff5420286f2398b9bf70ab26039ce2aaee3
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-10-17 22:03:41 -0400

    created migration CreateSkills

---
+++ hogwarts/db/migrate/20131018020322_create_skills.rb
@@ -0,0 +1,10 @@
+class CreateSkills < ActiveRecord::Migration
+  def change
+    create_table :skills do |t|
+      t.belongs_to :student, index: true
+      t.belongs_to :spell, index: true
+      t.belongs_to :teacher, index: true
+      t.integer :skill_level
+    end
+  end
+end

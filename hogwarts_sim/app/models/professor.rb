05a46f39f1f0f19e9307d70de66d6631a1a66366
Dan Crews <dancrews@me.com>
2013-10-18 08:48:18 -0400

    Created spell controller and model

--- hogwarts_sim/app/models/professor.rb
+++ hogwarts_sim/app/models/professor.rb
@@ -1,4 +1,6 @@
 class Professor < ActiveRecord::Base
       has_many :lectures
       has_many :students, through: :lectures
+      has_many :tags, as: :taggable
+
     end
--- hogwarts_sim/app/models/student.rb
+++ hogwarts_sim/app/models/student.rb
@@ -2,4 +2,5 @@ class Student < ActiveRecord::Base
   belongs_to :house
   has_and_belongs_to_many :lectures
   has_many :professors, through: :lectures
+  has_many :tags, as: :taggable
 end

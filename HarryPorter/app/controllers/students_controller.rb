250c978deb680d7584f2845de6c567f303dda409
Amal Hussein <amalhussein08@gmail.com>
2013-10-18 08:23:46 -0400

    Adding links, and cleaning up code

--- HarryPorter/app/controllers/students_controller.rb
+++ HarryPorter/app/controllers/students_controller.rb
@@ -49,7 +49,7 @@ class StudentsController < ApplicationController
   end 
 
   def student_params
-    params.require(:student).permit(:name, :house_id, :lecture_id)
+    params.require(:student).permit(:name, :house_id, :lecture_id, :teacher_id)
   end 
 end
 
--- HarryPorter/app/models/teacher.rb
+++ HarryPorter/app/models/teacher.rb
@@ -1,7 +1,6 @@
 class Teacher < ActiveRecord::Base 
 	has_many :lectures 
 	has_many :spells, through: :skills
-	has_many :spells, through: :skills 
 	has_many :skills 
 end 
 
